d5cefb985748c418585f179c444fdf838286a3ba
Eda Işıl İnal <edainalisil@gmail.com>
2024-01-22 13:34:36 +0300

    completed filtered

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { filteredItems } from '../redux/notes/notesSlice';
 
 
 function Filter() {
   const [search,setSearch] = useState("");
-
+const dispatch = useDispatch();
   const handleChange=(e)=> {
     setSearch(e.target.value);
+dispatch(filteredItems(e.target.value))
    
   }
   return (
--- src/redux/notes/notesSlice.jsx
+++ src/redux/notes/notesSlice.jsx
@@ -5,7 +5,8 @@ export const notesSlice = createSlice({
     name: 'notes',
     initialState: {
         items: [],
-        filteredItems: [],
+        originalItems: [], 
+     
     },
     reducers: {
         addText: (state, action) => {
@@ -14,13 +15,34 @@ export const notesSlice = createSlice({
                 text: action.payload.text,
                 background: action.payload.background,
             });
+            state.originalItems.push({
+                id: String(nextNoteId + 1),
+                text: action.payload.text,
+                background: action.payload.background,
+            });
+            
             nextNoteId += 1;
         },
         deleteItems: (state, action) => {
             const { id } = action.payload;
 
             state.items = state.items.filter(item => item.id !== id);
-        }
+            state.originalItems = state.originalItems.filter(item => item.id !== id);
+        },
+        filteredItems: (state, action) => {
+            const letter = action.payload.toLowerCase();
+            
+            if (!letter) {
+                state.items = state.originalItems;
+                return;
+              }
+            state.items = state.items.filter(item => {
+              if (typeof item.text !== 'string') {
+                return false;
+              }
+              return item.text.toLowerCase().startsWith(letter);
+            });
+          }
 
 
     },
